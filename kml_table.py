63887b13e1bec2eddc87080a9cd08c20c3d1f268
Riley Paul <rileypaul96@gmail.com>
2022-04-19 11:31:46 -0600

    Correct imports

--- kml_table.py
+++ kml_table.py
@@ -1,7 +1,8 @@
 import os
 
 def format_kml_table(data,**kwargs) -> str:
-  fname = os.path.join("kml_table_style.html")
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+  fname = os.path.join(dir_path,"kml_table_style.html")
   with open(fname,"r") as file: style = file.read()
   
   html = data.to_html(header=False,na_rep="-",**kwargs)
